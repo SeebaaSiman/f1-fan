2e8c707d1ffa7b4b15db5636ffc4ef3a4f179503
Sebastian <seebaasiman@gmail.com>
2023-06-08 02:42:06 -0300

    Esconder contentTeam

--- src/components/TwitterTimeLine.jsx
+++ src/components/TwitterTimeLine.jsx
@@ -1,5 +1,5 @@
-import styled from "styled-components";
 import { TwitterTimelineEmbed } from "react-twitter-embed";
+import styled from "styled-components";
 
 export const TwitterTimeLine = ({ screenName, tweetLimit }) => {
   return (
--- src/components/current-season/team/ContentTeam.jsx
+++ src/components/current-season/team/ContentTeam.jsx
@@ -7,14 +7,14 @@ export const ContentTeam = ({ id }) => {
 
   return (
     <div>
-      <div>las dos imágenes de los pilotos</div>
+      {/* <div>las dos imágenes de los pilotos</div>
       <div>container de información</div>
       <p>
         Full Team Name Aston Martin Aramco Cognizant F1 Team Base Silverstone,
         United Kingdom Team Chief Mike Krack Technical Chief Dan Fallows Chassis
         AMR23 Power Unit Mercedes First Team Entry 2018 World Championships N/A
         Highest Race Finish 1 (x1) Pole Positions 1 Fastest Laps N/A
-      </p>
+      </p> */}
     </div>
   );
 };
